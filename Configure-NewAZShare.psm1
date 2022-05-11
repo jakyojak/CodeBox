1d7d219f77747a4868e2892fea2c6e713d606f1c
jakyojak <66825105+jakyojak@users.noreply.github.com>
2022-05-11 12:47:30 +0100

    Create Configure-NewAZShare.psm1

---
+++ Configure-NewAZShare.psm1
@@ -0,0 +1,185 @@
+# Configures a default set of NTFS ACLs on a newly created Azure storage account share
+
+Function Configure-NewAZShare {
+  [CmdletBinding(SupportsShouldProcess)]
+  param (
+    [Parameter(Mandatory=$True,HelpMessage="The name of the storage account")] [string]$StorageAccount,
+    [Parameter(Mandatory=$True,HelpMessage="The name of the resourcegroup")] [string]$ResourceGroup,
+    [Parameter(Mandatory=$True,HelpMessage="The name of the file share")] [string]$Share,
+    [Parameter(Mandatory=$false)][string]$AdminGroup,
+    [Parameter(Mandatory=$false,HelpMessage="The name of the file share")][string]$Subscription)
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if ([string]::IsNullOrEmpty($(Get-AzContext).Account)) {Connect-AzAccount}
+
+if ([string]::IsNullOrEmpty($Subscription)) {#If context hasn't been specified
+                                             Write-Host "Parameter '-subscription' not set, defaulting to: uob-prd" -ForegroundColor Yellow
+                                             try {
+                                                  Set-AzContext "uob-prd" | Out-Null
+                                                  $AZcontext=(Get-AzContext).subscription.name
+                                                  }
+                                                  catch {Throw "Error setting context to subscription: uob-prd"}
+                                            }
+
+                                            else {#If context has been specified
+                                                  try {
+                                                       Set-AzContext $Subscription | Out-Null
+                                                       $AZcontext=(Get-AzContext).subscription.name
+                                                       }
+                                                       catch {Throw "Error setting context to subscription: $($subscription)"} 
+
+Write-Host "Finding resource group $($ResourceGroup) in $($AZcontext).." -NoNewline
+try {
+     $AZResourceGroup = Get-AzResourceGroup -Name $ResourceGroup -ea SilentlyContinue
+    }
+    catch { Throw "ERROR unable to find Resource Group: $ResourceGroup"}
+Write-Host "Done" -ForegroundColor Green
+#Write-Host $AZResourceGroup
+
+Write-Host "Finding storage account $($StorageAccount).." -NoNewline
+try {
+     $AZStorageAccount = Get-AzStorageAccount -ResourceGroupName $AZResourceGroup.ResourceGroupName -StorageAccountName $StorageAccount -ea SilentlyContinue
+    }
+    catch { Throw "ERROR unable to find Storage Account: $StorageAccount"}
+Write-Host "Done" -ForegroundColor Green
+
+#Write-Host "Applying permissions on share $($Share).."
+#Apply more permissions on the share (for full control access for admin groups)
+    $Gr_NASAdmins_Group = Get-AzADGroup -DisplayName "gr-nasadmins"
+    if (![string]::IsNullOrEmpty($($AdminGroup))) {
+    $AZAdminGroup= Get-AzADGroup -DisplayName $AdminGroup
+    }
+    
+Write-Host "Finding a valid kerberos key for temporary drive mapping.." -NoNewline
+try {
+     $key = (Get-AzStorageAccountKey -ResourceGroupName $AZResourceGroup.ResourceGroupName -StorageAccountName $AZStorageAccount.StorageAccountName | select -first 1).value
+     $Key_PW = $Key | ConvertTo-SecureString -AsPlainText -force
+    }
+    catch {Throw "ERROR getting kerberos key for: $StorageAccount" }
+Write-Host "Done" -ForegroundColor Green
+
+try {
+     $PS_Drive_params = @{ 
+         Name = "Z" #find an unused ps drive
+         PSProvider = "FileSystem"
+         Root = "\\$($AZStorageAccount.StorageAccountName).file.core.windows.net\$($Share)"
+         Persist = $false
+         Credential = [pscredential]::new("localhost\$($AZStorageAccount.StorageAccountName)",$Key_PW)
+     }
+
+        Write-Host "Mapping drive using keberos key.." -NoNewline
+        New-PSDrive @PS_Drive_params | Out-Null
+    }
+    catch { Throw "ERROR mapping drive: $StorageAccount"}
+Write-Host "Done" -ForegroundColor Green
+
+try {
+     Set-Location "$($PS_Drive_params.name):"
+    }
+    catch {Throw "ERROR Navigating to new drive mapping: $($PS_Drive_params.name)"}
+
+
+Write-Host "Setting ACLs on root of $($share) share for.."
+try {
+    $ACL=get-acl .
+    Write-Host "    $($Gr_NASAdmins_Group.displayname).." -NoNewline
+    $accessrule1 = New-Object System.Security.AccessControl.FileSystemAccessRule(
+          $Gr_NASAdmins_Group.displayname,
+          [System.Security.AccessControl.FileSystemRights]::FullControl,
+          (
+            [System.Security.AccessControl.InheritanceFlags]::ContainerInherit +
+            [System.Security.AccessControl.InheritanceFlags]::ObjectInherit
+          ),
+          [System.Security.AccessControl.PropagationFlags]::None,
+          [System.Security.AccessControl.AccessControlType]::Allow
+        )
+    $ACL.AddAccessRule($accessrule1)
+    Set-Acl -Path . $ACL
+    }
+    catch {Throw "ERROR setting ACLs for $($Gr_NASAdmins_Group.displayname)"}
+Write-Host "Done" -ForegroundColor Green
+Write-Host ""
+
+Write-Host "Removing unecessary ACLS.." -NoNewline
+try {
+    $NewACL1=get-acl .
+        $Rules=$NewACL1.GetAccessRules($true,$true, [System.Security.Principal.NTAccount])|? {($_.identityreference -ne "BUILTIN\Administrators") `
+                                                                                         -and ($_.identityreference -ne "CAMPUS`\gr-nasadmins") `
+                                                                                         -and ($_.identityreference -ne "NT AUTHORITY\SYSTEM")
+                                                                                                                        }
+        foreach ($rule in $rules) {$NewACL1.RemoveAccessRule($rule) | Out-Null}
+    
+    Set-Acl -Path . $NewACL1
+    }
+    catch {Throw "ERROR removing unecessary ACLs on root of share $($share)"}
+Write-Host "Done" -ForegroundColor Green
+
+
+Write-Host "Applying new ACLS for Authenticated Users.." -NoNewline
+try {
+    $NewACL2=get-acl .
+    $accessrule2 = New-Object System.Security.AccessControl.FileSystemAccessRule(
+          "NT AUTHORITY\Authenticated Users",
+          [System.Security.AccessControl.FileSystemRights]::ReadAndExecute,
+          (
+            [System.Security.AccessControl.InheritanceFlags]::None
+          ),
+          [System.Security.AccessControl.PropagationFlags]::None,
+          [System.Security.AccessControl.AccessControlType]::Allow
+        )
+   
+    
+    
+    $NewACL2.AddAccessRule($accessrule2)
+    Set-Acl -Path . $NewACL2
+    }
+    catch {Throw "Error applying new ACLS for $($accessrule2.IdentityReference.Value)"}
+Write-Host "Done" -ForegroundColor Green
+
+
+if (![string]::IsNullOrEmpty($($AdminGroup))) {
+Write-Host "Applying new ACLS for $($AZAdminGroup.DisplayName).." -NoNewline
+try {
+    $NewACL3=get-acl .
+    $accessrule3 = New-Object System.Security.AccessControl.FileSystemAccessRule(
+          $AZAdminGroup.DisplayName,
+          [System.Security.AccessControl.FileSystemRights]::FullControl,
+          (
+            [System.Security.AccessControl.InheritanceFlags]::ContainerInherit +
+            [System.Security.AccessControl.InheritanceFlags]::ObjectInherit
+          ),
+          [System.Security.AccessControl.PropagationFlags]::None,
+          [System.Security.AccessControl.AccessControlType]::Allow
+        )
+
+    $NewACL3.AddAccessRule($accessrule3)
+    Set-Acl -Path . $NewACL3
+    }
+    catch {Throw "Error applying new ACLS for $($accessrule3.IdentityReference.Value)"}
+Write-Host "Done" -ForegroundColor Green
+
+} else {Write-Host "WARNING: No xxgr-admin group defined. NO ACLs will be applied for IT supporter access" -ForegroundColor Yellow}
+        Write-Host ""
+   
+
+Write-Host "Navigating away from mapped drive $($PS_Drive_params.name).." -NoNewline
+try {
+    Set-Location C: #Unmap to prepare remove-PSDrive. Test if C: exists? maybe get system drive.
+    }
+    catch {Throw "ERROR unable to navigate away from temporary mapped drive $($PS_Drive_params.name)"}
+Write-Host "Done" -ForegroundColor Green
+
+
+Write-Host "Removing mapped drive $($PS_Drive_params.name).." -NoNewline
+try {
+    Remove-PSDrive -Name "$($PS_Drive_params.name)"
+    }
+    catch {Throw "ERROR unable to remove mapped drive $($PS_Drive_params.name)"}
+Write-Host "Done" -ForegroundColor Green
+Write-Host ""
+
+Write-Host "Task complete" -ForegroundColor Green
+}
+}
+#Export-ModuleMember -function New-ConfigureAZShare   
