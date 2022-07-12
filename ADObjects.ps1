c07cfac3d9b93b50f78a4ed076663cc096843349
jakyojak <66825105+jakyojak@users.noreply.github.com>
2022-07-12 15:07:00 +0100

    Update ADObjects.ps1

--- ADObjects.ps1
+++ ADObjects.ps1
@@ -1,13 +1,11 @@
-#Let's track AD object deletions. Ran daily.
-
 Write-Host "Any errors should show up here in the log"
 Write-Host ""
 $date=(get-date)
-[string]$CSV_ADObjects_Yesterday = "C:\Reports\ADObjects\ADObjects_Yesterday.csv"
+[string]$CSV_ADObjects_Yesterday = "C:\Reports\ADObjects\ADObjects_Yesterday.csv" #only used for reference
 [string]$CSV_ADObjects_Today = "C:\Reports\ADObjects\ADObjects_Today.csv"
 [string]$Deleted_ADObjects_CSV = "C:\Reports\ADObjects\ADObjects_Deleted.csv"
 
-$ADObjects_Yesterday = Import-Csv $CSV_ADObjects_Today #$CSV_ADObjectFiles_Today will be yesterday's results when this function next runs. This function only runs once per day.
+$ADObjects_Yesterday = Import-Csv $CSV_ADObjects_Today #$CSV_ADObjectFiles_Today will be yesterday's results when this script next runs. This script only runs once per day.
 
 $ADObjects = @()
 $User_Objects = Get-ADUser -filter {name -like "*"} -Properties name,sid,displayname,description,created | select name,sid,displayname,description,@{N="ObjType";E={"User"}},@{N="DateCreated";E={($_.created | get-date -Format "dd/MM/yyyy")}} | Sort-Object -Property name
@@ -16,7 +14,7 @@ $Computer_objects=  Get-ADComputer -filter {name -like "*"} -Properties name,sid
 
 $ADObjects+= $User_Objects
 $ADObjects+= $Group_Objects
-$ADObjects+=$Computer_objects
+$ADObjects+= $Computer_objects
 
 $ADObjects_Today = $ADObjects
 
@@ -33,9 +31,18 @@ $Comparison_joined = Join-Object -Left $ADObjects_Yesterday -Right $Comparison -
 If ($ADObjects_Missing.count -gt 0) {
                                     [array]$Deleted_ADObjects = Import-Csv $Deleted_ADObjects_CSV
                                     [array]$Deleted_ADObjects=[array]$Deleted_ADObjects+[array]$ADObjects_Missing
-                                    $Deleted_ADObjects | Sort-Object -Property DateDeleted -Descending | Export-Csv $Deleted_ADObjects_CSV -NoTypeInformation -Force
+
+                                    #-ErrorAction Stop on the line 43 will mean that if ADObjects_Deleted.csv doesn't get updated this will preserve the difference of 
+                                    # AD objects inside of ADObjects_Today.csv as the export won't run on line 43.
+                                    #On the next successful write of ADObjects_Deleted.csv the script will update ADObjects_Today.csv, albeit with offset DateDeleted Timestamps
+                                    #So, don't keep the ADObjects_Deleted.csv open in Excel as it will prevent this script from performing a successful write, or if you do, open it in
+                                    # read-only mode. Using notepad is fine.
+                                    $Deleted_ADObjects |% {$_.datedeleted = ([datetime]::Parse($_.datedeleted,([Globalization.CultureInfo]::CreateSpecificCulture('en-GB'))))}
+                                    $Deleted_ADObjects = $Deleted_ADObjects | Sort-Object -Property DateDeleted -Descending 
+                                    $Deleted_ADObjects |% {$_.datedeleted = ($_.datedeleted).ToShortDateString()}
+                                    $Deleted_ADObjects | Export-Csv $Deleted_ADObjects_CSV -NoTypeInformation -Force -ErrorAction Stop
                                     }
 
 
 $ADObjects_Yesterday | Export-Csv $CSV_ADObjects_Yesterday -NoTypeInformation -Force
-$ADObjects_Today | Export-Csv $CSV_ADObjects_Today -NoTypeInformation -Force
+$ADObjects_Today | Export-Csv $CSV_ADObjects_Today -NoTypeInformation -Force 
